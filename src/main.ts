1885b69e2d4c44346df1e09f958a74cba7a1192e
JeongYeonSeung <asas4654@daum.net>
2024-12-07 04:11:51 +0900

    fix: cors 설정

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Realthon 2024 API')
     .setVersion('1.0')
