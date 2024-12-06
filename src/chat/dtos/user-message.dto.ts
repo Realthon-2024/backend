6348f1a37b7369444538eabb3541da4d454c758c
125ryun <125ryun@gmail.com>
2024-12-07 05:33:11 +0900

    fix: UserMessageDto 커밋 추가

--- src/chat/dtos/user-message.dto.ts
+++ src/chat/dtos/user-message.dto.ts
@@ -1,10 +1,15 @@
 // src/chat/dtos/user-message.dto.ts
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString, } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString, } from "class-validator";
 
 export class UserMessageDto {
   @ApiProperty({ description: '사용자가 보낸 채팅 메시지 내용' })
   @IsString()
   @IsNotEmpty()
   content: string;
+
+  @ApiProperty({ description: '메시지가 속한 채팅방의 id' })
+  @IsNumber()
+  @IsNotEmpty()
+  chatRoomId: number;
 }
